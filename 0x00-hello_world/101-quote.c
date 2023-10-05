405ddb7c712191139d80e0eef1027686d2770570
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-05 19:17:55 +0100

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, sizeof(message) - 1);
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 	return (1);
 }
