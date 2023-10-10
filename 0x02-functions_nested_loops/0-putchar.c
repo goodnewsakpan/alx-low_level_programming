c301c8c5637eb3ea7b52fbe48abbbf9e75d87e65
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-10 15:48:42 +0100

    Add _putchar function and demonstration in main program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * main - Entry point
  * _putchar - writes the character c to stdout
  *
- * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
