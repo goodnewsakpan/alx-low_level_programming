9a734aceff743bc69180df49fbcd3840c30af81a
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-10 19:37:43 +0100

    function to print signs of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * print_sign - print the sign of a number.
+ * @n: print signs
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
+
