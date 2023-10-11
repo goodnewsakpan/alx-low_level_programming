8046ab9200b2530ae7f8664aa3c2170841ee4b17
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-11 06:02:44 +0100

    printing natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @c: the first number
+ */
+void print_to_98(int c)
+{
+	int i;
+
+	if (c <= 98)
+	{
+		for (i = c; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = c; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
