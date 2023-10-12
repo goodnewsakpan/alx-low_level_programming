d4ee23e24f1d8da4e71e09828ee6fe36df67a2e0
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-12 05:46:23 +0100

    print n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+{
+	int b, multi, r;
+		if (b <= 15 || b >= 0)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			_putchar('0');
+
+			for (multi = 1; multi <= n; multi++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				r = b * multi;
+				if (r <= 99)
+					_putchar(' ');
+
+				if (r <= 9)
+					_putchar(' ');
+				if (r >= 100)
+				{
+					_putchar((r / 100) + '0');
+					_putchar((r / 10) % 10 + '0');
+				}
+	else if (r <= 99 || r >= 10)
+	{
+		_putchar((r / 10) + '0');
+	}
+		}	_putchar((r % 10) + '0')
+		}
+_putchar('\n';
+			}
+	}
+}
