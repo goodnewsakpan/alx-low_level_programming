758b0ce5690b902d871d40fbebf56eb165d7adf8
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-16 17:19:11 +0100

    function to reset an int with 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - This task will reset int *n_to_98
+ * @n: value to be initialized
+ * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
