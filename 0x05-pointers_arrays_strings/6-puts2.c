e10f7309daeb7ca39c40dca41b9c7bf90f34b49a
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-17 23:27:37 +0100

    function to print every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,4 +17,6 @@ void puts2(char *str)
 	}
 
 	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - function to print half of a string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int q, start;
+
+	while (str[length] != '0')
+	{
+		length++;
+	}
+
+	if (length % 2 ==0)
+}
