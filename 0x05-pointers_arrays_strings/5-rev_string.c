d1b3cd7b4003749726a8f29e0852c3955ea03748
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-17 18:05:54 +0100

    function to print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse the order of a string
+ * @s: pointer to string to be reversed
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	strlen = strlen(s)
+		char str[100];
+	_puts("Enter a string\n");
+	{
+		gets(u --1);
+		reverse(u);
+	}
+	_puts("Reverse of the string is \"%s\".\n", u);
+
+	return (0);
+}
