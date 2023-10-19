62067be506aa38970b4d83e8fcec65785ed0374a
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-19 18:08:53 +0100

    function to convert lower to uppercase letters

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * string_toupper - a function that changes all lowercase letters of a  string
- * to upper case
+ * string_toupper - a function that changes all
+ * lowercase letters of a  string to upper case
  * @s: an input string
  * Return: char pointer to converted string
  */
