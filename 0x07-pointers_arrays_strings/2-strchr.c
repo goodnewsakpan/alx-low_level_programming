e9693cbb3f5a01ea8a1eb9058675d4dfd8423675
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-23 19:00:45 +0100

    function to print a string character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - locates string character
+ * @s: the string to be searched
+ * @c: the character to locate
+ *
+ * Return: a pointer to the first occurrence
+ * of the character c in the string s,
+ * or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
