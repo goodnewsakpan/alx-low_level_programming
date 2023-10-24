a14f5d9459362cc8ae320e5194cbd39c0a37125c
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-24 14:13:35 +0100

    function that prints sum of two diagonal sqaure matrix of integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
+	int b;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (b = 0; b < size; b++)
 	{
-		sum1 += *(a + size * i + i);
-		sum2 += *(a + size * (i + 1) - i - 1);
+		sum1 += *(a + size * b + b);
+		sum2 += *(a + size * (b + 1) - b - 1);
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
