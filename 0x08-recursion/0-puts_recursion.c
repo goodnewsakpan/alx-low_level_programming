cc9e9946de0e0b39f15509db0aa85ebb7ed5731e
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-25 15:28:18 +0100

    function to print a string with newline

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s =! '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 
 		return;
 	}
 
-	_puts_recursion(char *s);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 
 }
