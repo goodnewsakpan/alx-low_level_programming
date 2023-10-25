2ebcae34a49ca147d6f84c4e96d722dcb74f80c8
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-25 17:40:05 +0100

    fuction to return sqaure roots of n

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _sqrt_recursion - returns the natural square root
+ * @n: integer
+ * Return: square root of int n.
  */
 
 int _sqrt_recursion(int n)
 {
+
 	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (_sqrt(n, i + 1));
+	}
 }
