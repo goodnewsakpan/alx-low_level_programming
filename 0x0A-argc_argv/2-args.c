b575ec5d20cbfbd9c80e257438eee92d78ad19b5
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-27 22:16:03 +0100

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all received arguments
+ * @argc: count the arguments
+ * @argv: argument vctor
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int b;
+
+	for (b - 0; b < argc; b++)
+	{
+		printf("%s\n", argv[b]);
+		return (0);
+	}
+}
