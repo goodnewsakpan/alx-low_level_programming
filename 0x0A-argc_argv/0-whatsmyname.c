a6208de91287024d320afb3892158432bb23056e
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-27 19:12:18 +0100

    function to print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,26 @@
 #include "main.h"
+/**
+ * main - function returning an integer
+ * @argv: an array of pointers to the string of arguments
+ * @argc: a count of the arguments supplied
+ * Return: Always 0 (success).
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
 		int c = 0;
+
 		while (argv[0][c] != '\0')
 		{
 			_putchar(argv[0][c]);
 			c++;
 		}
-		_putchar('\n');
+		_putchar(' ', '\n');
+
 		return (0);
 
-		else
-		{
-			_putchar(' ');	
-		}
 	}
+
 }
