a7989790d8301e1f1181e5946caac4788d0a452d
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-10-30 09:50:18 +0100

    function that prints minimum coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,38 +11,38 @@
 
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+int money, coins = 0;
 
 if (argc != 2)
 {
 	printf("Error\n");
 	return (1);
 }
-cents = atoi(argv[1]);
-while (cents > 0)
+money = atoi(argv[1]);
+while (money > 0)
 {
 	coins++;
-	if ((cents - 25) >= 0)
+	if ((money - 25) >= 0)
 	{
-		cents -= 25;
+		money -= 25;
 		continue;
 	}
-	if ((cents - 10) >= 0)
+	if ((money - 10) >= 0)
 	{
-		cents -= 10;
+		money -= 10;
 		continue;
 	}
-	if ((cents - 5) >= 0)
+	if ((money - 5) >= 0)
 	{
-		cents -= 5;
+		money -= 5;
 		continue;
 	}
-	if ((cents - 2) >= 0)
+	if ((money - 2) >= 0)
 	{
-		cents -= 2;
+		money -= 2;
 		continue;
 	}
-	cents--;
+	money--;
 }
 printf("%d\n", coins);
 return (0);
