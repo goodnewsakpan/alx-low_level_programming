879715890afc62bda6f19efce4b89ab945ed98d7
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-02 17:12:52 +0100

    function to allocate memory of an array using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,24 +6,24 @@
  * @size: size of bytes.
  *
  * Return: pointer to the allocated memory.
- * if nmemb or size is 0, returns NULL.
- * if malloc fails, returns NULL.
+ * if nmemb or size is 0, return NULL.
+ * if malloc fails, calloc returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+	char *z;
+	unsigned int b;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
+	z = malloc(nmemb * size);
 
-	if (p == NULL)
+	if (z == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
+	for (b = 0; b < (nmemb * size); b++)
+		z[b] = 0;
 
-	return (p);
+	return (z);
 }
