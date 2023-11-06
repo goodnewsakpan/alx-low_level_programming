ba00bd6738327ba7503143af1566dbf6dd42481d
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-06 16:56:47 +0100

    defining struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct my_struct - Short description
+ * @char: name
+ * @float: age
+ * @char: owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
