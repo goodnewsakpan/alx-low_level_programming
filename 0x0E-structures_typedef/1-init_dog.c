52cf5793f77998ceaaaeb58dbb7c2ccb512cab4e
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-06 18:50:18 +0100

    function to initalize a variable struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,7 @@
  * @owner: the owner of the dog
  * @age: the age of the dog
  * @name: name of dog
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -16,4 +17,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
+	return (0);
 }
