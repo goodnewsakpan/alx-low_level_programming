1905302b207ee74217c7665fb2d2797a0b0e8e71
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-06 23:06:04 +0100

    change of name of dog file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int length = 0;
+char *ret;
+if (str == NULL)
+	return (NULL);
+	while (*str++)
+	length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+	return (NULL);
+	
