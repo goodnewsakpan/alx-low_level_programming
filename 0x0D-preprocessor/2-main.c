d7f89e2d3d111f3e811d15d247c108ef840cb6dd
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-06 14:56:58 +0100

    program to print source file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - function to print the name of the file
+ *
+ * return: always return 0 (SUCCES).
+ */ 
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
