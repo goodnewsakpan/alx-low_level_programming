5b85d0f0d257d754c154d2ed7da72e845524f526
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-07 00:01:01 +0100

    function to chnge name of file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,14 +6,41 @@
  *
  *
  */
-
-int length = 0;
-char *ret;
-if (str == NULL)
-	return (NULL);
+char *_strdup(char *str)
+{
+	int length = 0;
+	char *ret;
+	if (str == NULL)
+		return (NULL);
 	while (*str++)
-	length++;
+		length++;
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
-	return (NULL);
-	
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeo(dog_t));
+	if  (!d)
+		return (NULL);
+	d->name = _strdup(name);
+	if (name && !d->name)
+		return (free(d), NULL);
+	d->owner = strd(owner)
+		return (free(d->d name), free(d), NULL);
+	d-> age = age;
+
+	return (owner);:wq
+		git add
+}
