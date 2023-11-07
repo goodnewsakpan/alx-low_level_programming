ef632deda7e3e3944472c077312fd83b06372997
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-07 13:48:46 +0100

    function to chnge name of file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 /**
- * _strdup - recopies a string
- * @str: string to be replicated
- * Return: the string to the duplicate
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
  */
 char *_strdup(char *str)
 {
@@ -12,7 +13,6 @@ char *_strdup(char *str)
 	char *ret;
 
 	if (str == NULL)
-
 		return (NULL);
 	while (*str++)
 		length++;
@@ -23,28 +23,28 @@ char *_strdup(char *str)
 		ret[length] = *--str;
 	return (ret);
 }
-/**
- * new_dog - initializes new dog
- * @name: the name of dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- *
- * Return: pointer to a new dog
- */
 
+
+/**
+ * new_dog - instantiates a dawg
+ * @name: the dawgy name
+ * @age: the dawgy age
+ * @owner: the dawgy owner
+ *
+ * Return: pointer to new dawg.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 
-	if  (!d)
+	if (!d)
 		return (NULL);
 	d->name = _strdup(name);
 	if (name && !d->name)
 		return (free(d), NULL);
-	d->owner = _strd(owner);
+	d->owner = _strdup(owner);
 	if (owner && !d->owner)
 		return (free(d->name), free(d), NULL);
 	d->age = age;
-
 	return (d);
 }
