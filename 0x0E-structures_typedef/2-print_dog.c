64a199bb7de34d5b444a207eb374e31adf9bbce4
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-07 17:48:08 +0100

    function to print srtuct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- *
- *
- *
- *
- *
+ * print_dog - function to reassign names to struct
+ * @name: name of dog
+ * @age: float age of dog
+ * @owner: owner of dog
+ * @d: pointer to struct dog
  */
 
 void print_dog(struct dog *d)
@@ -13,8 +14,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %f\n", (*d).age ? d->age : "(nil)");
+	printf("Age: %f\n", (*d).age);
 	printf("owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
-	return (0);
 }
