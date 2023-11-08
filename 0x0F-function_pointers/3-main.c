747fd9540877a1200e140dcc09a6309730bf8c15
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-08 18:31:24 +0100

    main file for function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function to carry out basic arithmetic operations
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if ((*op == '/' || == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (op[1] != '\0' || get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
