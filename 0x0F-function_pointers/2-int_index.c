49ce207d559f9584c1a490d328fd0ccd6fbaef42
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-08 15:37:58 +0100

    function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for int
+ * @array: value for int
+ * @size: value
+ * @cmp: function pointer
+ */
+
+int int_idex(int *array, int size, int (*cmp)(int))
+{
+	int b = 0;
+	
+	if (cmp = NULL || array == NULL)
+		return (-1);
+	
+	for (b = 0; b < size; b++)
+	{
+		if (cmp(array[b]))
+			return (b);
+	}
+	return (-1);
+}	
+
