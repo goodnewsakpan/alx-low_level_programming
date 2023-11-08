84610ed538af7d93f927431e361a30f0b1b39f49
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-08 17:54:59 +0100

    functions that selects the correct function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - chooses appropriate function
+ * @s: the selected or not selected operation
+ * Return: pointer to the function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (ops[b].op != NULL && *(ops[b].op) != *s)
+	    b++;
+    return (ops[b].f);
+}
