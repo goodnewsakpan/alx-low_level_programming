45f568c2a00163244947ea746149228a989ff5a7
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-16 19:38:33 +0100

    function that prints all the elements of a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -23,4 +25,3 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
-
