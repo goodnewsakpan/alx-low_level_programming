df13cf737d45f0115e6aac1be752546ed6fb45c0
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-16 19:31:01 +0100

    function that prints all the elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * print_list - elements of a list
  * @h: the first element in the list
  * Return: return the elements of list_t
- *
  */
 
 size_t print_list(const list_t *h)
@@ -30,40 +26,4 @@ size_t print_list(const list_t *h)
 		node_count++;
 	}
 	return (node_count);
-}
-/**
- * main - Entry point
- * Return: return 0 on success
- *
- */
-
-int main(void)
-{
-	list_t *head;
-	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->str = strdup("Hello");
-	new->len = 5;
-	new->next = head;
-	head = new;
-	n = print_list(head);
-	printf("-> %lu elements\n", n);
-
-	printf("\n");
-	free(new->str);
-	new->str = NULL;
-	n = print_list(head);
-	printf("-> %lu elements\n", n);
-
-	free(new);
-	return (0);
 }
