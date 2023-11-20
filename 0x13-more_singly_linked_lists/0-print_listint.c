b01383fc5c18dacf2bdb4d0d2d9b8a4aab373c0f
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-20 18:25:58 +0100

    function that prints all elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 0;
-while (h != NULL)
-{
-	printf("%d\n", h->data);
-	node_count++;
-	h = h->new;
-}
-return (node_count);
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+
 }
