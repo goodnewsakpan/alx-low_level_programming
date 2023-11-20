aa7fed0940b1f6569b9941c2a3a74d646baa39d0
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-20 20:23:19 +0100

    function that prints all elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,18 +11,12 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s
+typedef struct listint_t
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_t *next;
+} listint_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
