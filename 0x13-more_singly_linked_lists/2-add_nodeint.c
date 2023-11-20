1d586fe78177e7287a5a5246c9a4c02b7601c8cf
Goodnews Akpan <goodnewsakpan123@gmail.com>
2023-11-20 20:44:14 +0100

    function that adds a new node at the beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,10 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
- *
+ * add_nodeint - function that adds anew node at the beginning of a list
+ * @n: a variable
+ * @head: pointer to the first element of the node
+ * Return: return pointer to the first element
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
